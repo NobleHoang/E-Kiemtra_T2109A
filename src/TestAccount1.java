dbf6a7edb79376fc45fd3ad2486e63e520687c88
NobleHoang <98103315+NobleHoang@users.noreply.github.com>
2022-05-11 15:51:40 +0700

    upadate

---
+++ src/TestAccount1.java
@@ -0,0 +1,9 @@
+public class TestAccount1 {
+    public static void main(String[] args) {
+        Account1 acc =new Account1();
+        acc.input();
+        acc.depositAndWithdraw();
+        acc.display();
+    }
+
+}
